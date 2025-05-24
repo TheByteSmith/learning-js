a2b5de8f2ede27a738ccea8e4efaa761d091eeca
TheByteSmith <205391583+TheByteSmith@users.noreply.github.com>
2025-05-24 10:13:41 +0530

    datatypes

---
+++ 02-dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+// console.log(3
+//     +
+//     3); console.log("Hitesh");
+
+// code readability reduced
+
+let name = "Hitesh"
+let age = 19
+let isLoggedIn = false
+
+// number = 2^53 (range)
+// bigint = 
+// string = ""
+// boolean = true / false
+// null = standalone value (representation of empty value)
+// undefined = value not defined
+// symbol = unique
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
