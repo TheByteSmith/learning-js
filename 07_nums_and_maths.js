437e433a5b15c5669482f3235ad39375da09649b
TheByteSmith <205391583+TheByteSmith@users.noreply.github.com>
2025-05-24 20:02:53 +0530

    nums and maths

---
+++ 07_nums_and_maths.js
@@ -0,0 +1,40 @@
+// const score = 400
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 1223.7834
+// console.log(otherNumber.toPrecision(3)); 
+// //returns a string!!!
+// // also round off the value to 3 (given in argument)
+// // 223.7834 => 224
+// // 1223.7834 => 1.22e+3
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString(en-IN));
+
+// =============== Maths ===============
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.5532)); //5
+// console.log(Math.ceil(4.1532)); //5 (upper value)
+// console.log(Math.floor(4.9532)); //4 (lower value)
+// console.log(Math.sqrt(1225)); //4
+// console.log(Math.min(2, 3, 3, 233,)); //4
+// console.log(Math.max(2, 3, 3, 233,)); //4
+
+console.log(Math.random()) // 0 to 1 any value
+console.log((Math.random()*10) + 1) 
+console.log(Math.floor(Math.random()*10) + 1) 
+// 
+// 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
