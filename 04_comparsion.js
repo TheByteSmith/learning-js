be6b24188794ce4297a25f13894e1f119cac23a2
TheByteSmith <205391583+TheByteSmith@users.noreply.github.com>
2025-05-24 11:29:48 +0530

    comparsions

---
+++ 04_comparsion.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// AVOID SUCH COMPARSIONS
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // F
+// console.log(null == 0);  // T
+// console.log(null >= 0); // F
+
+// console.log(undefined == 0); // F
+// console.log(undefined > 0); // F
+// console.log(undefined < 0); // F
+
+// === STRICT CHECK
+
+console.log("2" === 2);
