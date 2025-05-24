c4a171ced9966affa4a641c147c438e42faeb016
TheByteSmith <205391583+TheByteSmith@users.noreply.github.com>
2025-05-24 11:13:06 +0530

    operations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -23,7 +23,48 @@
 // "" = flase
 // "ad" = true
 
-let someNumber = 13
-let strinNumner = String(someNumber)
-console.log(strinNumner);
-console.log(typeof strinNumner);
+// let someNumber = 13
+// let strinNumner = String(someNumber)
+// console.log(strinNumner);
+// console.log(typeof strinNumner);
+
+//  =============== Operations =============== 
+
+let value = 3
+let negValue = -value
+// console.log(-value);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); // 2 raised to the power 3
+console.log(2/3);
+console.log(2%3); // remainder
+
+let str1 = "hellow"
+let str2 = "world"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + "2"); // 12
+console.log(1 + 2); // 3
+
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true);  // 1
+console.log(+""); // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+// shit
+
+let gameCounter = 100
+// gameCounter++
+// ++gameCounter
+console.log(gameCounter);
+
