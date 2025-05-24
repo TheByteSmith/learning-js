b4ffe002c2fb046ab32279e681ff21ba69c12583
TheByteSmith <205391583+TheByteSmith@users.noreply.github.com>
2025-05-24 10:38:04 +0530

    conversion of datatypes

---
+++ 03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score = 33
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+
+// Conversion
+// score -> conversion -> type
+// 33 = Number
+// 33abc = NaN
+// 
+// 
+// 
+
+// let isLoggedIn = "ad"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 = true; 0 = false
+// "" = flase
+// "ad" = true
+
+let someNumber = 13
+let strinNumner = String(someNumber)
+console.log(strinNumner);
+console.log(typeof strinNumner);
