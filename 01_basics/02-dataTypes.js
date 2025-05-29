0112f5d1d06bbb8ddab6f2816294033b77ee9665
TheByteSmith <205391583+TheByteSmith@users.noreply.github.com>
2025-05-29 13:12:16 +0530

    some brief explaination

--- 01_basics/02-dataTypes.js
+++ 01_basics/02-dataTypes.js
@@ -6,17 +6,20 @@
 //     +
 //     3); console.log("Hitesh");
 
-// code readability reduced
+// code readability reduced ⬆️
 
 let name = "Hitesh"
 let age = 19
 let isLoggedIn = false
 
 // number = 2^53 (range)
-// bigint = 
-// string = ""
-// boolean = true / false
+// bigint = whatever doesn't fit in number
+// string = "" anything in quotes
+// boolean = true / false, 1 / 0
 // null = standalone value (representation of empty value)
+
+// example: we don't get (temprature) value from server, so we will leave it empty beacuase we cannot use "0" if we didn't got the value. As, "0" in this case is a valid temprature.
+
 // undefined = value not defined
 // symbol = unique
 
@@ -25,3 +28,4 @@ let isLoggedIn = false
 console.log(typeof "hitesh");
 console.log(typeof null);
 console.log(typeof undefined);
+console.log(null)
