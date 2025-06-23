8be329d67bdc9fc1e3bd66a44c7739397eea137a
TheByteSmith <205391583+TheByteSmith@users.noreply.github.com>
2025-06-23 18:18:11 +0530

    while loops

---
+++ 05_iterations/02_while-loops.js
@@ -0,0 +1,26 @@
+// while : task is done BEFORE checking condition
+let i = 0
+while (i <= 10) {
+    console.log(`Value of i is ${i}`);
+    i = i + 2
+    // i = i + 3
+    // i = i + 1
+
+}
+
+let myArray = ["flash", "ironman", "batman"]
+
+let hero = 0
+while (hero < myArray.length) {
+    console.log(`Hero name is ${myArray[hero]}`)
+    hero = hero + 1
+}
+
+// do while : task is done AFTER checking condition
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10)
